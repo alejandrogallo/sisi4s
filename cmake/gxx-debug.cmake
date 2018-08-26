340cec304b237bb853ca8448dff061fd11f16921
Alejandro Gallo <aamsgallo@gmail.com>
2018-08-27 00:42:57 +0200

    Update cmake

--- cmake/gxx-debug.cmake
+++ cmake/gxx-debug.cmake
@@ -2,6 +2,8 @@ set(CMAKE_CXX_COMPILER mpicxx)
 
 include(lapack.cmake)
 include(scalapack.cmake)
+include(libint.cmake)
+include(eigen.cmake)
 
 set(
   OPTIMIZATION_FLAGS
@@ -16,13 +18,20 @@ set(
 
 set(
   LIBS
-  ctf scalapack lapack blas gfortran
+  ctf scalapack lapack blas gfortran int2
 )
 
 set(
   CC4S_LINK_DIRECTORIES
   ${SCALAPACK_LIB_DIR}
   ${LAPACK_LIB_DIR}
+  ${LIBINT_LIB_DIR}
+)
+
+set(
+  CC4S_INCLUDE_DIRECTORIES
+  ${LIBINT_INCLUDE_DIR}
+  ${EIGEN_INCLUDE_DIR}
 )
 
 set(
--- cmake/libint.cmake
+++ cmake/libint.cmake
@@ -1,21 +1,21 @@
 include(ExternalProject)
 
 ExternalProject_Add(libint_project
-GIT_REPOSITORY    https://github.com/evaleev/libint
-GIT_TAG           master
-PREFIX            ${CMAKE_BINARY_DIR}/lib/src/
+GIT_REPOSITORY    https://github.com/evaleev/libint.git
+GIT_TAG           v2.4.2
+PREFIX            ${CMAKE_BINARY_DIR}/lib/build/
 SOURCE_DIR        ${CMAKE_BINARY_DIR}/lib/src/libint
 BINARY_DIR        ${CMAKE_BINARY_DIR}/lib/src/libint
 UPDATE_COMMAND    ""
 CONFIGURE_COMMAND ${CMAKE_BINARY_DIR}/lib/src/libint/autogen.sh &&
-                  ${CMAKE_BINARY_DIR}/lib/src/libint/configure
+                  ${CMAKE_BINARY_DIR}/lib/src/libint/configure --prefix=${CMAKE_BINARY_DIR}/lib/build/libint
 BUILD_COMMAND     make
-INSTALL_COMMAND   ""
+INSTALL_COMMAND   make install
 TEST_COMMAND      ""
 )
 
 ExternalProject_Get_Property(libint_project SOURCE_DIR)
-ExternalProject_Get_Property(libint_project BINARY_DIR)
+ExternalProject_Get_Property(libint_project PREFIX)
 message("LIBINT SOURCE_DIR = ${SOURCE_DIR}")
-message("LIBINT BINARY_DIR = ${BINARY_DIR}")
-set(LIBINT_LIB_DIR ${BINARY_DIR}/lib)
+set(LIBINT_LIB_DIR ${PREFIX}/libint/lib)
+set(LIBINT_INCLUDE_DIR ${PREFIX}/libint/include)
