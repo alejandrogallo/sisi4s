1b4c5b3b1a61ece1285c885d4f2675baba37beaa
Alejandro Gallo <aamsgallo@gmail.com>
2019-04-05 16:37:42 +0200

    Add CcsdtEquationOfMotionDavidson

---
+++ src/algorithms/CcsdtEquationOfMotionDavidson.hpp
@@ -0,0 +1,32 @@
+#ifndef Ccsdt_EQUATION_OF_MOTION_DAVIDSON
+#define Ccsdt_EQUATION_OF_MOTION_DAVIDSON
+
+#include <algorithms/Algorithm.hpp>
+#include <math/FockVector.hpp>
+#include <vector>
+#include <math/Complex.hpp>
+
+namespace cc4s {
+
+  class CcsdtEquationOfMotionDavidson: public Algorithm {
+  public:
+    ALGORITHM_REGISTRAR_DECLARATION(CcsdtEquationOfMotionDavidson);
+    CcsdtEquationOfMotionDavidson(
+      std::vector<Argument> const &argumentList
+    );
+    virtual ~CcsdtEquationOfMotionDavidson();
+
+    virtual void run();
+
+    template<typename F>
+    void run();
+
+  protected:
+    static constexpr int DEFAULT_MAX_ITERATIONS = 16;
+
+  };
+
+}
+
+#endif
+
