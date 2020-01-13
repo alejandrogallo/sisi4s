9fb00494cbd63a579c182b0a2f49a1f7f7c16b12
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-13 18:56:13 +0100

    Export one body energies of hf

--- src/algorithms/HartreeFock.cxx
+++ src/algorithms/HartreeFock.cxx
@@ -1,3 +1,34 @@
+/*  BEGINNER's GUIDE TO LIBINT
+ *  --------------------------
+ *
+ * When we load a basis set, we get a Basis Set object, which consists
+ * of shells.
+ * Every shell has some basis functions inside (contracted gaussians),
+ * and these contracted gaussians are build out of uncontracted gaussian
+ * orbitals.
+ *
+ * The integrals are also in general computed shell-wise, for instance
+ * if s1 is the shell 1 of aug-cc-pvdz of Neon
+ *
+ *  S   8   1.00
+ *    17880.0000000              0.0007380
+ *     2683.0000000              0.0056770
+ *      611.5000000              0.0288830
+ *      173.5000000              0.1085400
+ *       56.6400000              0.2909070
+ *       20.4200000              0.4483240
+ *        7.8100000              0.2580260
+ *        1.6530000              0.0150630
+ *
+ * and s2 is the shell 4 of the same basis set
+ *
+ *  P   3   1.00
+ *       28.3900000              0.0460870
+ *        6.2700000              0.2401810
+ *        1.6950000              0.5087440
+ *
+ *
+ */
 #include <string>
 #include <vector>
 #include <libint2.hpp>
@@ -30,7 +61,7 @@ getNuclearRepulsionEnergy(std::vector<libint2::Atom>& structure)
       r2 += pow(structure[i].y - structure[j].y, 2);
       r2 += pow(structure[i].z - structure[j].z, 2);
       enuc += structure[i].atomic_number * structure[j].atomic_number
-               / sqrt(r2);
+              / sqrt(r2);
     }
   }
   return enuc;
@@ -39,7 +70,7 @@ getNuclearRepulsionEnergy(std::vector<libint2::Atom>& structure)
 
 Eigen::MatrixXd
 getOneBodyIntegrals(
-  const libint2::BasisSet shells,
+  const libint2::BasisSet& shells,
   libint2::Operator obtype,
   const std::vector<libint2::Atom>& atoms
   )
@@ -52,7 +83,6 @@ getOneBodyIntegrals(
   // construct the overlap integrals engine
   libint2::Engine engine(obtype, shells.max_nprim(), shells.max_l(), 0);
   // nuclear attraction ints engine needs to know where the charges sit ...
-  // the nuclei are charges in this case; in QM/MM there will also be classical charges
   if (obtype == libint2::Operator::nuclear) {
     std::vector<std::pair<double,std::array<double,3>>> q;
     for(const libint2::Atom& atom : atoms) {
@@ -65,33 +95,29 @@ getOneBodyIntegrals(
 
   const std::vector<size_t> shell2bf = shells.shell2bf();
 
-  // buf[0] points to the target shell set after every call  to
-  // engine.compute()
-  const auto& buf = engine.results();
+  const auto& resultBuffer = engine.results();
 
-  // loop over unique shell pairs, {s1,s2} such that s1 >= s2
-  // this is due to the permutational symmetry of the real integrals over
-  // Hermitian operators: (1|2) = (2|1)
-  for(size_t s1=0; s1!=shells.size(); ++s1) {
+  // loop over unique shell pairs s1>=s2
+  for(size_t s1 = 0; s1 != shells.size(); ++s1) {
 
     auto bf1 = shell2bf[s1]; // first basis function in this shell
-    auto n1 = shells[s1].size();
+    auto n1 = shells[s1].size(); // size of the shell (how many cgs are in it)
 
-    for(size_t s2=0; s2<=s1; ++s2) {
+    for(size_t s2 = 0; s2 <= s1; ++s2) {
 
       auto bf2 = shell2bf[s2];
       auto n2 = shells[s2].size();
 
-      // compute shell pair; return is the pointer to the buffer
       engine.compute(shells[s1], shells[s2]);
 
-      // "map" buffer to a const Eigen Matrix, and copy it to the corresponding
-      // blocks of the result
-      Eigen::Map<const Eigen::MatrixXd> buf_mat(buf[0], n1, n2);
-      result.block(bf1, bf2, n1, n2) = buf_mat;
-      if (s1 != s2) // if s1 >= s2, copy {s1,s2} to the corresponding {s2,s1}
-      //block, note the transpose!
-      result.block(bf2, bf1, n2, n1) = buf_mat.transpose();
+      // copy the result buffer of size (n1*n2) to the
+      // bufferMatrix, needed to do a block assignment to the Eigen matrix
+      Eigen::Map<const Eigen::MatrixXd> bufferMatrix(resultBuffer[0], n1, n2);
+      result.block(bf1, bf2, n1, n2) = bufferMatrix;
+      if (s1 != s2) {
+        // transpose and copy to the non-explicitly calculated block
+        result.block(bf2, bf1, n2, n1) = bufferMatrix.transpose();
+      }
 
     }
   }
@@ -100,73 +126,82 @@ getOneBodyIntegrals(
 }
 
 
+/*
 Eigen::MatrixXd
-getTwoBodyFock(
+getTwoBodyMatrices(
   const libint2::BasisSet shells,
   const Eigen::MatrixXd& D
   ) {
+}
+*/
 
-  using libint2::Shell;
-  using libint2::Engine;
-  using libint2::Operator;
+Eigen::MatrixXd
+getTwoBodyFock(
+  const libint2::BasisSet& shells,
+  const Eigen::MatrixXd& D
+  ) {
 
-  const auto n = shells.nbf();
-  Eigen::MatrixXd G = Eigen::MatrixXd::Zero(n,n);
+  const auto n = shells.nbf();  // number of basis functions
+  Eigen::MatrixXd G = Eigen::MatrixXd::Zero(n,n);  // result matrix
 
-  // construct the electron repulsion integrals engine
+  // turn on the engine
   libint2::Engine engine(
     libint2::Operator::coulomb,
     shells.max_nprim(),
     shells.max_l(),
-    0
-  );
+    0);
 
+  // shell to its first basis function map
   const std::vector<size_t> shell2bf = shells.shell2bf();
 
-  // buf[0] points to the target shell set after every call  to engine.compute()
-  const auto& buf = engine.results();
+  // resultBuffer[0] points to the target shell set after every call to
+  // engine.compute()
+  const auto& resultBuffer = engine.results();
 
   // loop over shell pairs of the Fock matrix, {s1,s2}
-  // Fock matrix is symmetric, but skipping it here for simplicity (see compute_2body_fock)
-  for(size_t s1=0; s1!=shells.size(); ++s1) {
+  // Fock matrix is symmetric, but skipping it here for simplicity
+  for(size_t s1 = 0; s1 != shells.size(); ++s1) {
 
     auto bf1_first = shell2bf[s1]; // first basis function in this shell
     auto n1 = shells[s1].size();
 
-    for(size_t s2=0; s2!=shells.size(); ++s2) {
+    for(size_t s2 = 0; s2 != shells.size(); ++s2) {
 
       auto bf2_first = shell2bf[s2];
       auto n2 = shells[s2].size();
 
       // loop over shell pairs of the density matrix, {s3,s4}
       // again symmetry is not used for simplicity
-      for(size_t s3=0; s3!=shells.size(); ++s3) {
+      for(size_t s3 = 0; s3 != shells.size(); ++s3) {
 
         auto bf3_first = shell2bf[s3];
         auto n3 = shells[s3].size();
 
-        for(size_t s4=0; s4!=shells.size(); ++s4) {
+        for(size_t s4 = 0; s4 != shells.size(); ++s4) {
 
           auto bf4_first = shell2bf[s4];
           auto n4 = shells[s4].size();
 
-          // Coulomb contribution to the Fock matrix is from {s1,s2,s3,s4} integrals
+          // Coulomb contribution to the Fock matrix is from {s1,s2,s3,s4}
+          // integrals
           engine.compute(shells[s1], shells[s2], shells[s3], shells[s4]);
-          const auto* buf_1234 = buf[0];
+          const auto* buf_1234 = resultBuffer[0];
           if (buf_1234 == nullptr)
             continue; // if all integrals screened out, skip to next quartet
 
-          // we don't have an analog of Eigen for tensors (yet ... see github.com/BTAS/BTAS, under development)
+          // we don't have an analog of Eigen for tensors (yet ... see
+          // github.com/BTAS/BTAS, under development)
           // hence some manual labor here:
-          // 1) loop over every integral in the shell set (= nested loops over basis functions in each shell)
-          // and 2) add contribution from each integral
-          for(size_t f1=0, f1234=0; f1!=n1; ++f1) {
+          // 1) loop over every integral in the shell set (= nested loops over
+          //    basis functions in each shell)
+          // 2) add contribution from each integral
+          for(size_t f1 = 0, f1234 = 0; f1 != n1; ++f1) {
             const auto bf1 = f1 + bf1_first;
-            for(size_t f2=0; f2!=n2; ++f2) {
+            for(size_t f2 = 0; f2 != n2; ++f2) {
               const auto bf2 = f2 + bf2_first;
-              for(size_t f3=0; f3!=n3; ++f3) {
+              for(size_t f3 = 0; f3 != n3; ++f3) {
                 const auto bf3 = f3 + bf3_first;
-                for(size_t f4=0; f4!=n4; ++f4, ++f1234) {
+                for(size_t f4 = 0; f4 != n4; ++f4, ++f1234) {
                   const auto bf4 = f4 + bf4_first;
                   G(bf1,bf2) += D(bf3,bf4) * 2.0 * buf_1234[f1234];
                 }
@@ -174,17 +209,18 @@ getTwoBodyFock(
             }
           }
 
-          // exchange contribution to the Fock matrix is from {s1,s3,s2,s4} integrals
+          // exchange contribution to the Fock matrix is from {s1,s3,s2,s4}
+          // integrals
           engine.compute(shells[s1], shells[s3], shells[s2], shells[s4]);
-          const auto* buf_1324 = buf[0];
+          const auto* buf_1324 = resultBuffer[0];
 
-          for(size_t f1=0, f1324=0; f1!=n1; ++f1) {
+          for(size_t f1 = 0, f1324 = 0; f1 != n1; ++f1) {
             const auto bf1 = f1 + bf1_first;
-            for(size_t f3=0; f3!=n3; ++f3) {
+            for(size_t f3 = 0; f3 != n3; ++f3) {
               const auto bf3 = f3 + bf3_first;
-              for(size_t f2=0; f2!=n2; ++f2) {
+              for(size_t f2 = 0; f2 != n2; ++f2) {
                 const auto bf2 = f2 + bf2_first;
-                for(size_t f4=0; f4!=n4; ++f4, ++f1324) {
+                for(size_t f4 = 0; f4 != n4; ++f4, ++f1324) {
                   const auto bf4 = f4 + bf4_first;
                   G(bf1,bf2) -= D(bf3,bf4) * buf_1324[f1324];
                 }
@@ -216,7 +252,6 @@ void HartreeFock::run() {
 
   LOG(1, "HartreeFock") << "maxIterations: " << maxIterations  << std::endl;
   LOG(1, "HartreeFock") << "ediff: " << electronicConvergence << std::endl;
-  LOG(1, "HartreeFock") << "Basis set: " << basisSet << std::endl;
 
   // Initialize libint
   LOG(1, "HartreeFock") << "libint2: " << LIBINT_VERSION << std::endl;
@@ -227,9 +262,6 @@ void HartreeFock::run() {
   std::ifstream structureFileStream(xyzStructureFile.c_str());
   std::vector<libint2::Atom> atoms(libint2::read_dotxyz(structureFileStream));
   structureFileStream.close();
-
-  libint2::BasisSet shells(basisSet, atoms);
-  nBasisFunctions = shells.nbf();
 
   if (numberOfElectrons == -1) {
     numberOfElectrons = 0;
@@ -246,46 +278,51 @@ void HartreeFock::run() {
   LOG(1, "HartreeFock") << "nelec: " << numberOfElectrons << std::endl;
   LOG(1, "HartreeFock") << "No: " << No << std::endl;
 
+  // initializing basis set and outputing relevant information
+  LOG(1, "HartreeFock") << "basis: " << basisSet << std::endl;
+  libint2::BasisSet shells(basisSet, atoms);
+  nBasisFunctions = shells.nbf();
+  LOG(1, "HartreeFock") << "Initializing basis set.." << std::endl;
+  LOG(1, "HartreeFock") << "#shells: " << shells.size() << std::endl;
+  LOG(1, "HartreeFock") << "#functions: " << nBasisFunctions << std::endl;
+  LOG(1, "HartreeFock") << "max_l: " << shells.max_l() << std::endl;
+  LOG(1, "HartreeFock")
+    << "Max functions in shell = " << shells.max_nprim() << std::endl;
+
+  LOG(1, "HartreeFock") << "shell: l\tAOS\tCGS" << std::endl;
+  for (auto &shell: shells) {
+    LOG(1, "HartreeFock") << "shell: "
+      << shell.size() << "\t"
+      << shell.nprim() << "\t"
+      << shell.ncontr() << "\t"
+      << std::endl;
+  }
+
   enuc = getNuclearRepulsionEnergy(atoms);
   LOG(1, "HartreeFock") << "nuclear energy: " << enuc << std::endl;
-
-  LOG(1, "HartreeFock") << "Initializing basis set.." << std::endl;
-
-  LOG(1, "HartreeFock") << "#basis: " << nBasisFunctions << std::endl;
-  LOG(1, "HartreeFock")
-    << "Maximum primitives in shells = " << shells.max_nprim()
-    << std::endl;
-  LOG(1, "HartreeFock") << "max_l: " << shells.max_l() << std::endl;
 
   LOG(1, "HartreeFock") << "Calculating overlaps" << std::endl;
   Eigen::MatrixXd S = getOneBodyIntegrals(
     shells,
     libint2::Operator::overlap, atoms);
-  //OUT() << S << std::endl;
-
-  LOG(1, "HartreeFock") << "shell: l ao cg" << std::endl;
-  for (auto &shell: shells) {
-    LOG(1, "HartreeFock") << "shell: "
-      << shell.size() << " "
-      << shell.nprim() << " "
-      << shell.ncontr() << " "
-      << std::endl;
-  }
 
   LOG(1, "HartreeFock") << "Calculating kinetic integrals" << std::endl;
   Eigen::MatrixXd T = getOneBodyIntegrals(
     shells,
     libint2::Operator::kinetic,
     atoms);
-  LOG(1, "HartreeFock") << "T(" << T.rows() << "," << T.cols() << ")" << std::endl;
+  LOG(1, "HartreeFock")
+    << "T(" << T.rows() << "," << T.cols() << ")" << std::endl;
 
 
-  LOG(1, "HartreeFock") << "Compute nuclear repulsion integrals" << std::endl;
+  LOG(1, "HartreeFock")
+    << "Compute nuclear repulsion integrals" << std::endl;
   Eigen::MatrixXd V = getOneBodyIntegrals(
     shells,
     libint2::Operator::nuclear,
     atoms);
-  LOG(1, "HartreeFock") << "V(" << V.rows() << "," << V.cols() << ")" << std::endl;
+  LOG(1, "HartreeFock")
+    << "V(" << V.rows() << "," << V.cols() << ")" << std::endl;
 
   LOG(1, "HartreeFock") << "Calculating the core hamiltonian" << std::endl;
   Eigen::MatrixXd H = T + V;
@@ -300,21 +337,19 @@ void HartreeFock::run() {
 
   D *= 0.0;
 
+  LOG(1, "HartreeFock") << "Setting initial density matrix" << std::endl;
   for ( i=0 ; i < nBasisFunctions ; i++) {
     for (unsigned j=i ; j < i+1 ; j++) {
       D(i,j) = 1;
     }
   }
 
-  LOG(1, "HartreeFock") << "Initial Density Matrix:" << std::endl;
-  //OUT() << D << std::endl;
-
-
   unsigned int iter(0);
   double rmsd(0);
   double energyDifference(0);
   double ehf(0);
   double ehfLast(0);
+  Eigen::MatrixXd eps;
 
   do {
 
@@ -327,15 +362,29 @@ void HartreeFock::run() {
     F += getTwoBodyFock(shells, D);
 
     // solve F C = e S C
-    Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd> gen_eig_solver(F, S);
-    auto eps(gen_eig_solver.eigenvalues());
+    Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd>
+      gen_eig_solver(F, S);
+    eps = gen_eig_solver.eigenvalues();
+
     // C now has all eigenvectors from the shell of course
     auto C(gen_eig_solver.eigenvectors());
+    /*
+    for (unsigned int ii(0) ; ii < C.cols() ; ii++) {
+      for (unsigned int jj(0) ; jj < C.rows() ; jj++) {
+        float nn(0);
+        for (unsigned int kk(0) ; kk < C.rows() ; kk++) {
+          nn += C(kk,ii) * C(kk, jj);
+        }
+        LOG(1, "HartreeFock") << ii << "|" << jj << " = " << nn << std::endl;
+      }
+    }
+    */
 
     // Computer the new density D = C(occ) . C(occ)T
     auto C_occ(C.leftCols(No));
     D = C_occ * C_occ.transpose();
 
+    // Compute the current hartree fock energy for the current density matrix
     ehf = 0.0;
     for (unsigned i=0 ; i < nBasisFunctions; i++) {
       for (unsigned j=0 ; j < nBasisFunctions ; j++) {
@@ -347,22 +396,21 @@ void HartreeFock::run() {
     rmsd = (D - D_last).norm();
 
     if (iter == 1) {
-      LOG(0, "HartreeFock") <<
+      LOG(1, "HartreeFockIt") << std::setprecision(15) << std::setw(10) <<
         "Iter" << "\t" <<
-        "E(elec)" << "\t" <<
-        "E(tot)" << "\t" <<
-        "Delta(E)" << "\t" <<
+        "E" << "\t" <<
+        "DeltaE" << "\t" <<
         "RMS(D)"
         << std::endl;
     }
 
-    LOG(0, "HartreeFock") << std::setprecision(15) << std::setw(10) <<
-        iter << "\t" <<
-        ehf  << "\t" <<
-        ehf + enuc << "\t" <<
-        energyDifference << "\t" <<
-        rmsd << "\t" <<
-        std::endl;
+    LOG(1, "HartreeFockIt") <<
+      iter << "\t" <<
+      ehf  << "\t" <<
+      energyDifference << "\t" <<
+      rmsd << "\t" <<
+    std::endl;
+
 
   } while (
       ((fabs(energyDifference) > electronicConvergence) ||
@@ -370,8 +418,13 @@ void HartreeFock::run() {
       (iter < maxIterations)
       );
 
-  LOG(0, "HartreeFock") << "energy=" << ehf + enuc << std::endl;
+  for (unsigned int e; e<eps.size(); e++) {
+    LOG(1, "HartreeFock")
+      << "band " << e + 1 << " = " << eps(e,0) << std::endl;
+  }
 
-  libint2::finalize();  // do not use libint after this
+  LOG(1, "HartreeFock") << "energy=" << ehf + enuc << std::endl;
+
+  libint2::finalize();
 
 }
