881e65da96ae65db09d759200e82e28c42b755ad
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-27 15:56:48 +0100

    Gaussian: use integrals utils

--- src/algorithms/CoulombIntegralsFromGaussian.cxx
+++ src/algorithms/CoulombIntegralsFromGaussian.cxx
@@ -6,6 +6,7 @@
 #include <ctf.hpp>
 #include <Cc4s.hpp>
 #include <util/Log.hpp>
+#include <util/Integrals.hpp>
 #include <iostream>
 #include <Eigen/Eigenvalues>
 #include <ctf.hpp>
@@ -28,35 +29,6 @@ struct ShellInfo {
     begin = shells.shell2bf()[i];
     end = size + begin;
     l = shells[i].contr[0].l;
-  }
-};
-
-// general enum for calculating different parts of the integrals
-enum Index { NO, NV, NP };
-
-template <typename A>
-A permuteIndices(const A& a, size_t i, size_t j) {
-  A b(a); b[i] = a[j]; b[j] = a[i]; return b;
-}
-
-// struct used to store information about the names, indices ranges
-// and string indices for ctf. It computes also IntegralInfos for
-// antisymmetry options
-struct IntegralInfo {
-  const std::string name;
-  const std::array<Index, 4> indices;
-  const std::string ids;
-  IntegralInfo(std::string n, std::array<Index, 4> i, std::string is):
-    name(n), indices(i), ids(is){}
-  std::vector<IntegralInfo> getAntisymmetrizers() const {
-    std::vector<IntegralInfo> result;
-    for (auto i: std::vector<int>({0,2})) {
-      std::string newName(permuteIndices(name, i, i+1));
-      std::array<Index, 4> newIndices(permuteIndices(indices, i, i+1));
-      std::string newIds(permuteIndices(ids, i, i+1));
-      result.push_back({newName, newIndices, newIds});
-    }
-    return result;
   }
 };
 
