503f430e6275d9a6a94042ec399650d43d6be066
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-29 10:46:01 +0100

    Clean up antisymmetrizer

--- src/algorithms/TensorAntisymmetrizer.cxx
+++ src/algorithms/TensorAntisymmetrizer.cxx
@@ -18,22 +18,23 @@ ALGORITHM_REGISTRAR_DEFINITION(TensorAntisymmetrizer);
 
 void TensorAntisymmetrizer::run() {
   const std::vector<IntegralInfo> infos({
-    {"HHHHCoulombIntegrals", {NO,NO,NO,NO}, "ijkl"},
-    {"HHHPCoulombIntegrals", {NO,NO,NO,NV}, "ijka"},
-    {"HHPHCoulombIntegrals", {NO,NO,NV,NO}, "ijak"},
-    {"HHPPCoulombIntegrals", {NO,NO,NV,NV}, "ijab"},
-    {"HPHHCoulombIntegrals", {NO,NV,NO,NO}, "iajk"},
     {"HPHPCoulombIntegrals", {NO,NV,NO,NV}, "iajb"},
-    {"HPPHCoulombIntegrals", {NO,NV,NV,NO}, "iabj"},
-    {"HPPPCoulombIntegrals", {NO,NV,NV,NV}, "iabc"},
-    {"PHHHCoulombIntegrals", {NV,NO,NO,NO}, "aijk"},
     {"PHHPCoulombIntegrals", {NV,NO,NO,NV}, "aijb"},
     {"PHPHCoulombIntegrals", {NV,NO,NV,NO}, "aibj"},
-    {"PHPPCoulombIntegrals", {NV,NO,NV,NV}, "aibc"},
-    {"PPHHCoulombIntegrals", {NV,NV,NO,NO}, "abij"},
+    {"HPPHCoulombIntegrals", {NO,NV,NV,NO}, "iabj"},
+    // trivially self antisymmetrizable
+    {"HHPHCoulombIntegrals", {NO,NO,NV,NO}, "ijak"},
+    {"HHPPCoulombIntegrals", {NO,NO,NV,NV}, "ijab"},
     {"PPHPCoulombIntegrals", {NV,NV,NO,NV}, "abic"},
-    {"PPPHCoulombIntegrals", {NV,NV,NV,NO}, "abci"},
+    {"PPHHCoulombIntegrals", {NV,NV,NO,NO}, "abij"},
+    {"HHHHCoulombIntegrals", {NO,NO,NO,NO}, "ijkl"},
+    {"HHHPCoulombIntegrals", {NO,NO,NO,NV}, "ijka"},
     {"PPPPCoulombIntegrals", {NV,NV,NV,NV}, "abcd"},
+    {"PPPHCoulombIntegrals", {NV,NV,NV,NO}, "abci"},
+    {"HPHHCoulombIntegrals", {NO,NV,NO,NO}, "iajk"},
+    {"PHHHCoulombIntegrals", {NV,NO,NO,NO}, "aijk"},
+    {"HPPPCoulombIntegrals", {NO,NV,NV,NV}, "iabc"},
+    {"PHPPCoulombIntegrals", {NV,NO,NV,NV}, "aibc"},
   });
   std::set<std::string> antiSet;
   for (const auto &integral : infos) {
@@ -43,9 +44,9 @@ void TensorAntisymmetrizer::run() {
         // we can use antigral since it has been computed and it has not
         // been initialized
         LOG(1, "TensorAntisymmetrizer")
-          << "Anti symmetrizing " << integral.name
-          << " with " << antigral.name << std::endl;
-        LOG(1, "TensorAntisymmetrizer")
+          << integral.name
+          << " from " << antigral.name << std::endl;
+        LOG(1, "TensorAntisymmetrizer") << "  "
           << integral.name << "[" << integral.ids << "] -= "
           << antigral.name << "[" << antigral.ids << "]"
           << std::endl;
@@ -64,6 +65,11 @@ void TensorAntisymmetrizer::run() {
         << "error: " 
         << integral.name << " could not be antisymmetrized"
         << std::endl;
+      LOG(1, "TensorAntisymmetrizer")
+        << "error: I tried the following: " << std::endl;
+      for (const auto& antigral: integral.getAntisymmetrizers()) {
+        LOG(1, "TensorAntisymmetrizer") << "> " << antigral.name << std::endl;
+      }
       throw new EXCEPTION("Antisymmetrization error");
     }
   }
