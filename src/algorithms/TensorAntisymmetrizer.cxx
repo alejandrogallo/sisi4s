f255c7150b20434de7741be0bb5fd84f2eb63fd3
Alejandro Gallo <aamsgallo@gmail.com>
2020-01-30 23:38:39 +0100

    TensorAntisymmetrizer: now working copying all integrals

--- src/algorithms/TensorAntisymmetrizer.cxx
+++ src/algorithms/TensorAntisymmetrizer.cxx
@@ -1,10 +1,12 @@
 #include <algorithms/TensorAntisymmetrizer.hpp>
 #include <string>
 #include <vector>
+#include <math/MathFunctions.hpp>
 #include <algorithm>
 #include <ctf.hpp>
 #include <Cc4s.hpp>
 #include <util/Log.hpp>
+#include <util/SharedPointer.hpp>
 #include <util/Integrals.hpp>
 #include <iostream>
 #include <ctf.hpp>
@@ -18,29 +20,47 @@ ALGORITHM_REGISTRAR_DEFINITION(TensorAntisymmetrizer);
 
 void TensorAntisymmetrizer::run() {
   const std::vector<IntegralInfo> infos({
-    {"HPHPCoulombIntegrals", {NO,NV,NO,NV}, "iajb"},
-    {"PHHPCoulombIntegrals", {NV,NO,NO,NV}, "aijb"},
-    {"PHPHCoulombIntegrals", {NV,NO,NV,NO}, "aibj"},
-    {"HPPHCoulombIntegrals", {NO,NV,NV,NO}, "iabj"},
-    // trivially self antisymmetrizable
-    {"HHPHCoulombIntegrals", {NO,NO,NV,NO}, "ijak"},
-    {"HHPPCoulombIntegrals", {NO,NO,NV,NV}, "ijab"},
-    {"PPHPCoulombIntegrals", {NV,NV,NO,NV}, "abic"},
-    {"PPHHCoulombIntegrals", {NV,NV,NO,NO}, "abij"},
     {"HHHHCoulombIntegrals", {NO,NO,NO,NO}, "ijkl"},
     {"HHHPCoulombIntegrals", {NO,NO,NO,NV}, "ijka"},
-    {"PPPPCoulombIntegrals", {NV,NV,NV,NV}, "abcd"},
-    {"PPPHCoulombIntegrals", {NV,NV,NV,NO}, "abci"},
+    {"HHPHCoulombIntegrals", {NO,NO,NV,NO}, "ijak"},
+    {"HHPPCoulombIntegrals", {NO,NO,NV,NV}, "ijab"},
     {"HPHHCoulombIntegrals", {NO,NV,NO,NO}, "iajk"},
-    {"PHHHCoulombIntegrals", {NV,NO,NO,NO}, "aijk"},
+    {"HPHPCoulombIntegrals", {NO,NV,NO,NV}, "iajb"},
+    {"HPPHCoulombIntegrals", {NO,NV,NV,NO}, "iabj"},
     {"HPPPCoulombIntegrals", {NO,NV,NV,NV}, "iabc"},
+    {"PHHHCoulombIntegrals", {NV,NO,NO,NO}, "aijk"},
+    {"PHHPCoulombIntegrals", {NV,NO,NO,NV}, "aijb"},
+    {"PHPHCoulombIntegrals", {NV,NO,NV,NO}, "aibj"},
     {"PHPPCoulombIntegrals", {NV,NO,NV,NV}, "aibc"},
+    {"PPHHCoulombIntegrals", {NV,NV,NO,NO}, "abij"},
+    {"PPHPCoulombIntegrals", {NV,NV,NO,NV}, "abic"},
+    {"PPPHCoulombIntegrals", {NV,NV,NV,NO}, "abci"},
+    {"PPPPCoulombIntegrals", {NV,NV,NV,NV}, "abcd"},
   });
-  std::set<std::string> antiSet;
+  std::map<std::string, PTR(CTF::Tensor<double>)> integralCopies;
   for (const auto &integral : infos) {
+  if ( isArgumentGiven(integral.name) ) {
+    LOG(1, "TensorAntisymmetrizer") << "Copying " << integral.name
+      << std::endl;
+    integralCopies[integral.name] = NEW(
+      CTF::Tensor<double>,
+      *getTensorArgument<double>(integral.name));
+  }
+  }
+
+  // debug
+  for (auto& copy: integralCopies) {
+    double norm(frobeniusNorm(*copy.second));
+    LOG(1, "TensorAntisymmetrizerNorm") << "|" << copy.first << "| = "
+      << norm << std::endl;
+  }
+
+  for (const auto &integral : infos) {
+    bool antisymmetrized(false);
     if ( ! isArgumentGiven(integral.name) ) continue;
+
     for (const auto& antigral: integral.getAntisymmetrizers()) {
-      if (isArgumentGiven(antigral.name) && !antiSet.count(antigral.name)) {
+      if (isArgumentGiven(antigral.name)) {
         // we can use antigral since it has been computed and it has not
         // been initialized
         LOG(1, "TensorAntisymmetrizer")
@@ -51,22 +71,24 @@ void TensorAntisymmetrizer::run() {
           << antigral.name << "[" << antigral.ids << "]"
           << std::endl;
         auto inteCtf(getTensorArgument<double>(integral.name));
-        auto antiCtf(getTensorArgument<double>(antigral.name));
+        //auto aantiCtf(getTensorArgument<double>(antigral.name));
+        auto antiCtf(integralCopies[antigral.name]);
         (*inteCtf)[integral.ids.data()] -= (*antiCtf)[antigral.ids.data()];
-        antiSet.insert(integral.name);
+        antisymmetrized = true;
+        // go to the next integral
         break;
       }
-
     }
+
     // if integral.name is not antisymmetrized, then something went
     // wrong, we don't have enough integrals to do this, panic!
-    if (!antiSet.count(integral.name)) {
+    if (!antisymmetrized) {
       LOG(1, "TensorAntisymmetrizer")
         << "error: " 
         << integral.name << " could not be antisymmetrized"
         << std::endl;
       LOG(1, "TensorAntisymmetrizer")
-        << "error: I tried the following: " << std::endl;
+        << "error: possibilities: " << std::endl;
       for (const auto& antigral: integral.getAntisymmetrizers()) {
         LOG(1, "TensorAntisymmetrizer") << "> " << antigral.name << std::endl;
       }
@@ -74,5 +96,10 @@ void TensorAntisymmetrizer::run() {
     }
   }
 
+  for (auto& copy: integralCopies) {
+    double norm(frobeniusNorm(*copy.second));
+    LOG(1, "TensorAntisymmetrizerNorm") << "|" << copy.first << "| = "
+      << norm << std::endl;
+  }
 
 }
