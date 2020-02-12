be79da786193c7623797599041b945e88e183061
Alejandro Gallo <aamsgallo@gmail.com>
2020-02-12 19:20:29 +0100

    CoulombIntegralsFromGaussian: Add test for fast implementation

--- src/algorithms/CoulombIntegralsFromGaussian.cxx
+++ src/algorithms/CoulombIntegralsFromGaussian.cxx
@@ -126,6 +126,89 @@ struct IntegralProvider {
 
   }
 
+  // This is a faster version of the obvious implementation
+  std::vector<double> compute_fast(Index P, Index Q, Index R, Index S) {
+    // < P Q | R S > = (P R | Q S)
+
+    compute_Vklmn();
+
+    const Limit pLim(indexToLimits(P)),
+                qLim(indexToLimits(Q)),
+                rLim(indexToLimits(R)),
+                sLim(indexToLimits(S));
+
+    const size_t dimension(pLim.size * qLim.size * rLim.size * sLim.size);
+    std::vector<double> result(dimension, 0.0);
+
+    for (size_t s(sLim.lower); s < sLim.upper; ++s) {
+    for (size_t q(qLim.lower); q < qLim.upper; ++q) {
+    for (size_t r(rLim.lower); r < rLim.upper; ++r) {
+    for (size_t p(pLim.lower); p < pLim.upper; ++p) {
+
+      //  <p q | r s> = (p r , q s)
+      const int IPQRS(
+        pLim[p] +
+        qLim[q] * pLim.size +
+        rLim[r] * pLim.size*qLim.size +
+        sLim[s] * pLim.size*qLim.size*rLim.size);
+
+      std::vector<double> Vpmlk(
+        Np*Np*Np* pLim.size, 0.0);
+      std::vector<double> Vprlk(
+        Np*Np*    rLim.size*pLim.size, 0.0);
+      std::vector<double> Vprqk(
+        Np*       qLim.size*rLim.size*pLim.size, 0.0);
+
+      // we will define four intermediates
+      for (size_t k(0), Inmlk = 0; k < Np; ++k) {
+
+        const size_t Iprqk(
+          pLim[p] +
+          rLim[r] * pLim.size +
+          qLim[q] * pLim.size*rLim.size +
+          k       * pLim.size*rLim.size*pLim.size);
+
+        result.data()[IPQRS] += C[k + s*Np] * Vprqk[Iprqk];
+
+      for (size_t l(0); l < Np; ++l) {
+
+        const size_t Iprlk(
+          pLim[p] +
+          rLim[r] * pLim.size +
+          l       * pLim.size*rLim.size +
+          k       * pLim.size*rLim.size*Np);
+
+        Vprqk[Iprqk] += C[l + q*Np] * Vprlk[Iprlk];
+
+      for (size_t m(0); m < Np; ++m) {
+
+        const size_t Ipmlk(
+          pLim[p] +
+          m * pLim.size +
+          l * pLim.size*Np +
+          k * pLim.size*Np*Np);
+
+        Vprlk[Iprlk] += C[m + r*Np] * Vpmlk[Ipmlk];
+
+      for (size_t n(0); n < Np; ++n, ++Inmlk) {
+
+        //  Vnmlk
+        Vpmlk[Ipmlk] += C[n + p*Np] * Vklmn[Inmlk];
+
+      } // n
+      } // m
+      } // l
+      } // k
+
+    } // s
+    } // r
+    } // q
+    } // p
+
+    return result;
+
+  }
+
   std::vector<double> compute(Index P, Index Q, Index R, Index S) {
     // < P Q | R S > = (P R | Q S)
 
@@ -257,7 +340,14 @@ void CoulombIntegralsFromGaussian::run() {
     LOG(1, "CoulombIntegralsFromGaussian")
       << "Computing " <<  integral.name << std::endl;
 
-    std::vector<double> result(engine.compute(i[0], i[1], i[2], i[3]));
+    std::vector<double> result;
+    if (!isArgumentGiven("fast")) {
+      LOG(1, "CoulombIntegralsFromGaussian")
+        << "Computing slow implementation" << std::endl;
+      result = engine.compute(i[0], i[1], i[2], i[3]);
+    } else {
+      result = engine.compute_fast(i[0], i[1], i[2], i[3]);
+    }
     std::vector<int> lens(4);
     for (unsigned int j(0) ; j < 4 ; j++) {
       lens[j] = (int)engine.indexToInt(i[j]);
