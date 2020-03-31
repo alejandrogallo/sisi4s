eb5fb3335ab83598b2d665caa5a8f028dd86ce79
Alejandro Gallo <aamsgallo@gmail.com>
2020-03-31 19:44:21 +0200

    Clean up FockMatrixFromCoulombIntegrals

--- src/algorithms/FockMatrixFromCoulombIntegrals.cxx
+++ src/algorithms/FockMatrixFromCoulombIntegrals.cxx
@@ -13,6 +13,7 @@
 #include <numeric>
 #include <map>
 #include <set>
+#define LOGGER(_l) LOG(_l, "FockMatrixFromCoulombIntegrals")
 
 using namespace cc4s;
 
@@ -22,14 +23,15 @@ void FockMatrixFromCoulombIntegrals::run() {
 
   const bool unrestricted(getIntegerArgument("unrestricted", 0) == 1);
 
-  LOG(0, "FockMatrixFromCoulombIntegrals")
-    << "unrestricted? " << unrestricted
-    << std::endl;
+  LOGGER(0) << "unrestricted? " << unrestricted << std::endl;
 
   const auto phph(getTensorArgument<double>("PHPHCoulombIntegrals"));
   int No(phph->lens[1]), Nv(phph->lens[0]);
   std::vector<int> vv({Nv, Nv}), oo({No, No}), ov({No, Nv}), vo({Nv, No});
   std::vector<int> syms({NS, NS});
+
+  LOGGER(0) << "No: " << No << std::endl;
+  LOGGER(0) << "Nv: " << Nv << std::endl;
 
   // ij: HH HHHH
   auto fij(new CTF::Tensor<double>(2, oo.data(), syms.data(), *Cc4s::world));
@@ -44,7 +46,7 @@ void FockMatrixFromCoulombIntegrals::run() {
   auto fab(new CTF::Tensor<double>(2, vv.data(), syms.data(), *Cc4s::world));
   const auto pp(getTensorArgument<double>("PPMatrix"));
   const auto phhp(getTensorArgument<double>("PHHPCoulombIntegrals"));
-  (*fab)["ab"] = (*pp)["ab"];
+  (*fab)["ab"]  = (*pp)["ab"];
   (*fab)["ab"] += (unrestricted ? 1.0 : 2.0)*(*phph)["akbk"];
   (*fab)["ab"] += (-1.0)*(*phhp)["akkb"];
   allocatedTensorArgument<double>("PPFockMatrix", fab);
@@ -53,7 +55,7 @@ void FockMatrixFromCoulombIntegrals::run() {
   auto fai(new CTF::Tensor<double>(2, vo.data(), syms.data(), *Cc4s::world));
   const auto ph(getTensorArgument<double>("PHMatrix"));
   const auto phhh(getTensorArgument<double>("PHHHCoulombIntegrals"));
-  (*fai)["ai"] = (*ph)["ai"];
+  (*fai)["ai"]  = (*ph)["ai"];
   (*fai)["ai"] += (unrestricted ? 1.0 : 2.0)*(*phhh)["akik"];
   (*fai)["ai"] += (-1.0)*(*phhh)["akki"];
   allocatedTensorArgument<double>("PHFockMatrix", fai);
@@ -78,6 +80,6 @@ void FockMatrixFromCoulombIntegrals::run() {
   const double dEnergy(energy.get_val());
 
   LOG(0, "FockMatrixFromCoulombIntegrals")
-    << "energie = " << dEnergy << std::endl;
+    << "energy= " << dEnergy << std::endl;
 
 }
