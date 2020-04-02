ab17e01b15e8ebf0febb5c2a2c072209b6bbc887
Alejandro Gallo <aamsgallo@gmail.com>
2020-04-02 13:53:08 +0200

    Add OneBodyFromGaussian

---
+++ src/algorithms/OneBodyFromGaussian.hpp
@@ -0,0 +1,27 @@
+#ifndef _ONE_BODY_FROM_GAUSSIAN_DEFINED
+#define _ONE_BODY_FROM_GAUSSIAN_DEFINED
+
+#include <algorithms/Algorithm.hpp>
+#include <vector>
+#include <math/Complex.hpp>
+#include <libint2.hpp>
+#include <Eigen/Eigen>
+
+namespace cc4s {
+
+  class OneBodyFromGaussian: public Algorithm {
+  public:
+    ALGORITHM_REGISTRAR_DECLARATION(OneBodyFromGaussian);
+    OneBodyFromGaussian(
+      std::vector<Argument> const &argumentList
+    ): Algorithm(argumentList) {}
+    ~OneBodyFromGaussian() {}
+    virtual void run();
+  };
+
+
+
+}
+
+#endif
+
