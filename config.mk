c285632fa2a1230d56461dcc76fa8d3e147ccca3
Alejandro Gallo <aamsgallo@gmail.com>
2020-04-07 13:30:15 +0200

    Improve building

--- config.mk
+++ config.mk
@@ -5,7 +5,8 @@ HF ?= yes
 THERMAL ?= no
 
 # destination path for installation
-INSTALL = ~/bin/cc4s/${CONFIG}
+INSTALL ?= ~/bin/cc4s/${CONFIG}
+TARGET ?= Cc4s
 
 ifeq ($(HF), yes)
 include etc/make/eigen.mk
@@ -14,7 +15,9 @@ endif
 
 include etc/make/yaml.mk
 include etc/make/ctf.mk
-include etc/make/blas.mk
-include etc/make/scalapack.mk
 
+ifdef INTEL_COMPILER
+LIBS += ${LIBS_STATIC} ${LIBS_DYNAMIC}
+else
 LIBS += -Wl,-Bstatic ${LIBS_STATIC} -Wl,-Bdynamic ${LIBS_DYNAMIC}
+endif
