9d7411ace5a285f921ba910c83846febc0e2eb17
Alejandro Gallo <aamsgallo@gmail.com>
2020-04-20 16:11:36 +0200

    Add parser library and XyzParser

---
+++ src/util/Parsing.hpp
@@ -0,0 +1,55 @@
+#ifndef _ALEJANDROS_PARSING_LIBRAY
+#define _ALEJANDROS_PARSING_LIBRAY
+
+#include<regex>
+#include<string>
+#include<vector>
+
+namespace pars {
+  using Str = std::string;
+  const
+  Str oneOrMore("+")
+    , newline("\\n")
+    , anyChar(".")
+    , tab("\\t")
+    , anyOf("*")
+    , optional("?")
+    , eof("$")
+    , orOf("|")
+    , bof("^") // begin of line
+    , alnum("[[:alnum:]]") // lowercase letters, uppercase letters, and digits
+    , alpha("[[:alpha:]]") // lowercase letters and uppercase letters
+    , blank("[[:blank:]]") // space or tab
+    , cntrl("[[:cntrl:]]") // the file format escape characters
+    , digit("[[:digit:]]") // digits
+   // lowercase letters, uppercase letters, digits, and punctuation
+    , graph("[[:graph:]]")
+    , lower("[[:lower:]]") // lowercase letters
+    , upper("[[:upper:]]")  // uppercase characters
+   // lowercase letters, uppercase letters, digits, punctuation, and space
+    , print("[[:print:]]")
+    , punct("[[:punct:]]") // punctuation
+    , space("[[:space:]]") // space
+    , xdigit("[[:xdigit:]]")  // hexadecimal digits (with upper and lower)
+    , realNumber("[-eE.+\\d]+") // real number TODO: improve
+    ;
+
+  const std::function<Str(Str)>
+    capture([](const Str& i) { return "(" + i + ")"; })
+    ;
+
+  Str oneOf(const std::vector<Str> &v) {
+    return std::accumulate
+      ( v.begin()
+      , v.end()
+      , std::string("")
+      , std::function<Str(Str, Str)>(
+          [](const Str &a, const Str &b){ return a.size() ? a + orOf + b : b; }
+          )
+      )
+      ;
+  }
+
+}
+
+#endif
---
+++ src/util/XyzParser.hpp
@@ -0,0 +1,67 @@
+#ifndef _XYZ_PARSER_
+#define _XYZ_PARSER_
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <util/Parsing.hpp>
+#include <regex>
+
+using namespace pars;
+
+struct Atom {
+  const std::string symbol;
+  const struct { double x, y, z; } position;
+};
+
+struct XyzParser {
+  const
+  std::string atom_numbers = digit + oneOrMore
+            , sep = blank + oneOrMore // any number > 1 of spaces or tabs
+            , atom = upper + lower + optional // atom is Upper char and lower char
+            , xyz_line = bof + blank + anyOf // spaces at the start allowed
+                       + capture(atom) + sep // capture atom symbol
+                       + capture(realNumber) + sep
+                       + capture(realNumber) + sep
+                       + capture(realNumber)
+                       + anyChar + anyOf + eof
+            ;
+
+  Atom parseLine(const std::string &line) {
+    std::smatch m;
+    std::regex_match(line, m, std::regex(xyz_line));
+    if (m.size() == 0) { throw EXCEPTION("Line " + line + " is malformed"); }
+    return { std::string(m[1])
+           , { std::atof(std::string(m[2]).c_str())
+             , std::atof(std::string(m[3]).c_str())
+             , std::atof(std::string(m[3]).c_str())
+             }
+           };
+  }
+
+  std::vector<Atom> parseFile(const std::string &fileName) {
+    std::fstream f(fileName);
+    std::string line;
+    std::vector<Atom> atoms;
+    if (!f.is_open()) throw EXCEPTION("File IO error: " + fileName);
+
+    std::getline(f, line); // Number of atoms
+    const int natoms = std::atoi(line.c_str());
+
+    std::getline(f, line); // Title of xyz file
+
+    // parse lines
+    for (int i(0); i<natoms; i++){
+      std::getline(f, line);
+      atoms.push_back(parseLine(line));
+      std::cout << atom[-1] << std::endl;
+    }
+
+    return atoms;
+  }
+
+};
+
+#endif
