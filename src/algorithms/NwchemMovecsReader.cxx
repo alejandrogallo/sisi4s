968640301b8d13a5cb942916532c542518736e87
Alejandro Gallo <aamsgallo@gmail.com>
2020-04-28 18:59:03 +0200

    Move MovecsParser to nwchem folder

--- src/algorithms/NwchemMovecsReader.cxx
+++ src/algorithms/NwchemMovecsReader.cxx
@@ -12,7 +12,8 @@
 #include <util/Emitter.hpp>
 #include <util/Parsing.hpp>
 #include <util/XyzParser.hpp>
-#include <util/NwchemBasisParser.hpp>
+#include <nwchem/BasisParser.hpp>
+#include <nwchem/MovecsParser.hpp>
 #include <regex>
 #define LOGGER(_l) LOG(_l, "NwchemMovecsReader")
 #define IF_GIVEN(_l, ...) if (isArgumentGiven(_l)) { __VA_ARGS__ }
@@ -21,81 +22,6 @@ using namespace cc4s;
 
 ALGORITHM_REGISTRAR_DEFINITION(NwchemMovecsReader);
 
-template <typename F>
-std::vector<F> readFortranChunk(std::fstream &file) {
-  uint32_t fortran_section, copy;
-  std::vector<F> buffer;
-
-  file.read((char*)&fortran_section, sizeof(int32_t));
-    copy = fortran_section;
-    buffer.resize(fortran_section / sizeof(F));
-    file.read((char*)buffer.data(), fortran_section);
-  file.read((char*)&fortran_section, sizeof(int32_t));
-  assert(copy == fortran_section);
-
-  return buffer;
-}
-
-
-struct MovecReader {
-  size_t nsets, Np;
-  std::vector<size_t> nmo;
-  std::vector<double> occupations, eigenvalues, mos;
-  size_t iset = 0; // This allows to skip the first set, dont need this yet
-  MovecReader(const std::string &fileName) {
-    std::fstream file(fileName.c_str(), std::ios::binary | std::ios::in);
-    readFortranChunk<char>(file); // convergence info
-    readFortranChunk<char>(file); // scftype
-    readFortranChunk<char>(file); // lentit
-    LOGGER(0) << "title: " << readFortranChunk<char>(file).data() << std::endl;
-    readFortranChunk<char>(file).data(); // lenbas
-    LOGGER(0) << "basis: " << readFortranChunk<char>(file).data() << std::endl;
-
-    nsets = *readFortranChunk<int64_t>(file).data(); // nsets
-    Np = *readFortranChunk<int64_t>(file).data();   // Np
-
-    LOGGER(0) << "nsets: " << nsets << std::endl;
-    LOGGER(0) << "Np: " << Np << std::endl;
-
-    // read nmos
-    for (size_t i=0; i<nsets; i++) {
-      int64_t buf = *readFortranChunk<int64_t>(file).data();
-      nmo.push_back(buf);
-      LOGGER(0) << "nmo[" << i << "]: " << nmo[0] << std::endl;
-    }
-
-    // loop which reject MOs from the set
-    for (size_t i = 0; i < iset;  i++) {
-      readFortranChunk<char>(file);
-      readFortranChunk<char>(file);
-      for (uint64_t j=0; j<nmo[i]; j++) { //do i = 1, nmo(jset) read(unitno)
-        readFortranChunk<char>(file);
-      }
-    }
-
-    for (size_t s(0); s < nsets; s++) { // do s = 1, nsets
-
-      // Read occupation numbers
-      occupations = readFortranChunk<double>(file);
-      LOGGER(0) << "#occupations read: " << occupations.size() << std::endl;
-      assert(occupations.size() == Np);
-
-      // Eigenvaluess [read(unitno) (evals(j), j=1,Np)]
-      eigenvalues = readFortranChunk<double>(file);
-      LOGGER(0) << "#eigenvalues read: " <<  eigenvalues.size() << std::endl;
-      assert(eigenvalues.size() == Np);
-
-      mos.resize(Np * Np);
-      for (size_t i(0); i < nmo[iset]; i++) { // do i = 1, nmo(iset)
-        std::vector<double> mos_buff(Np);
-        mos_buff = readFortranChunk<double>(file);
-        assert(mos_buff.size() == Np);
-        for (size_t j(0); j < mos_buff.size(); j++)
-          mos[j + Np*i] = mos_buff[j];
-      }
-    }
-  }
-};
 
 std::vector< std::vector<size_t> > findShellIndices
   ( const nwchem::BasisSet &bs
@@ -137,7 +63,7 @@ void NwchemMovecsReader::run() {
                   ;
   const int No(getIntegerArgument("No"));
 
-  // get <shell>Scaling
+  // make <shell>Scaling
   const auto _s
     = [&](std::string s) { return
         std::make_pair< nwchem::am::AngularMomentum
@@ -147,7 +73,7 @@ void NwchemMovecsReader::run() {
                             this->getTextArgument(s+ "Scaling", ""))
                         );
         };
-  // get <shell>Reorder
+  // make <shell>Reorder
   const auto _r
     = [&](std::string s) { return
         std::make_pair< nwchem::am::AngularMomentum
@@ -185,7 +111,7 @@ void NwchemMovecsReader::run() {
               << a.position.z << std::endl;
   }
 
-  const MovecReader movec(fileName);
+  const nwchem::MovecReader movec(fileName);
   std::vector<double> mos(movec.Np * movec.Np);
   mos = movec.mos;
 
@@ -237,10 +163,6 @@ void NwchemMovecsReader::run() {
       LOGGER(0) << "done with "  << am << std::endl;
     }
   }
-
-  /* TODO: reorder
-  for (const auto &p: transformation.reorder) { ...  }
-  */
 
   std::vector<int> pp(2, movec.Np), syms(2, NS), o(1, No), v(1, movec.Np - No);
   std::vector<int64_t> ids;
---
+++ src/nwchem/MovecsParser.hpp
@@ -0,0 +1,88 @@
+#ifndef _MOVECS_PARSER_FOR_NWCHEM_HEADER__
+#define _MOVECS_PARSER_FOR_NWCHEM_HEADER__
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <util/Log.hpp>
+#define _L(_l) LOG(_l, "MovecReader")
+
+template <typename F>
+std::vector<F> readFortranChunk(std::fstream &file) {
+  uint32_t fortran_section, copy;
+  std::vector<F> buffer;
+
+  file.read((char*)&fortran_section, sizeof(int32_t));
+    copy = fortran_section;
+    buffer.resize(fortran_section / sizeof(F));
+    file.read((char*)buffer.data(), fortran_section);
+  file.read((char*)&fortran_section, sizeof(int32_t));
+  assert(copy == fortran_section);
+
+  return buffer;
+}
+
+namespace nwchem {
+
+struct MovecReader {
+  size_t nsets, Np;
+  std::vector<size_t> nmo;
+  std::vector<double> occupations, eigenvalues, mos;
+  size_t iset = 0; // This allows to skip the first set, dont need this yet
+  MovecReader(const std::string &fileName) {
+    std::fstream file(fileName.c_str(), std::ios::binary | std::ios::in);
+    readFortranChunk<char>(file); // convergence info
+    readFortranChunk<char>(file); // scftype
+    readFortranChunk<char>(file); // lentit
+    _L(0) << "title: " << readFortranChunk<char>(file).data() << std::endl;
+    readFortranChunk<char>(file).data(); // lenbas
+    _L(0) << "basis: " << readFortranChunk<char>(file).data() << std::endl;
+
+    nsets = *readFortranChunk<int64_t>(file).data(); // nsets
+    Np = *readFortranChunk<int64_t>(file).data();   // Np
+
+    _L(0) << "nsets: " << nsets << std::endl;
+    _L(0) << "Np: " << Np << std::endl;
+
+    // read nmos
+    for (size_t i=0; i<nsets; i++) {
+      int64_t buf = *readFortranChunk<int64_t>(file).data();
+      nmo.push_back(buf);
+      _L(0) << "nmo[" << i << "]: " << nmo[0] << std::endl;
+    }
+
+    // loop which reject MOs from the set
+    for (size_t i = 0; i < iset;  i++) {
+      readFortranChunk<char>(file);
+      readFortranChunk<char>(file);
+      for (uint64_t j=0; j<nmo[i]; j++) { //do i = 1, nmo(jset) read(unitno)
+        readFortranChunk<char>(file);
+      }
+    }
+
+    for (size_t s(0); s < nsets; s++) { // do s = 1, nsets
+
+      // Read occupation numbers
+      occupations = readFortranChunk<double>(file);
+      _L(0) << "#occupations read: " << occupations.size() << std::endl;
+      assert(occupations.size() == Np);
+
+      // Eigenvaluess [read(unitno) (evals(j), j=1,Np)]
+      eigenvalues = readFortranChunk<double>(file);
+      _L(0) << "#eigenvalues read: " <<  eigenvalues.size() << std::endl;
+      assert(eigenvalues.size() == Np);
+
+      mos.resize(Np * Np);
+      for (size_t i(0); i < nmo[iset]; i++) { // do i = 1, nmo(iset)
+        const std::vector<double> mos_buff(readFortranChunk<double>(file));
+        assert(mos_buff.size() == Np);
+        for (size_t j(0); j < mos_buff.size(); j++)
+          mos[j + Np*i] = mos_buff[j];
+      }
+    }
+  }
+};
+
+}
+
+#endif
