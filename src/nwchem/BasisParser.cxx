fe9f3db2b1f2fb8a8a72efd56bfd37668a26c0ae
Alejandro Gallo <aamsgallo@gmail.com>
2020-06-08 19:42:17 +0200

    Outsource parts of BasisParser for nwchem

--- src/nwchem/BasisParser.cxx
+++ src/nwchem/BasisParser.cxx
@@ -1,22 +1,11 @@
 #include <nwchem/BasisParser.hpp>
+#include <util/AngularMomentum.hpp>
+#include <util/BasisSet.hpp>
 #include <numeric>
 
 using namespace pars;
 using namespace nwchem;
 using namespace am;
-
-std::vector<am::AngularMomentum> am::all() {
-  return { S, P, D, F, G, H, I, K };
-}
-size_t am::toInt (const am::AngularMomentum &a) { return a; }
-
-am::AngularMomentum am::fromString(const std::string &a) {
-  if (a == "S") return S; if (a == "P") return P;
-  if (a == "D") return D; if (a == "F") return F;
-  if (a == "G") return G; if (a == "H") return H;
-  if (a == "I") return I; if (a == "K") return K;
-  throw "I don't anderstand symbol: " + a;
-}
 
 const Regex spherical = oneOf({ "SPHERICAL", "spherical" })
           , no_line   = bof + blank + anyOf + eof
--- src/nwchem/BasisParser.hpp
+++ src/nwchem/BasisParser.hpp
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <map>
 #include <util/Parsing.hpp>
+#include <util/AngularMomentum.hpp>
+#include <util/BasisSet.hpp>
 #include <regex>
 #include <numeric>
 
@@ -14,44 +16,6 @@
 namespace nwchem {
 
   using namespace pars;
-
-  struct ContractedGaussian {
-    const std::vector<double> coefficients, exponents;
-    size_t size() const { return coefficients.size(); }
-  };
-
-  namespace am {
-    enum AngularMomentum { S = 1, P = 3 , D = 5 , F = 7
-                         , G = 9, H = 11, I = 13, K = 15
-                         };
-    std::vector<AngularMomentum> all();
-    size_t toInt (const AngularMomentum &);
-    AngularMomentum fromString(const std::string &);
-  }
-
-  struct Shell {
-    const std::string atom;
-    const am::AngularMomentum am;
-    const ContractedGaussian g;
-    size_t size() const { return g.size(); }
-  };
-
-  struct Basis {
-    const std::string atom;
-    const std::string name;
-    const std::vector<Shell> shells;
-    size_t size() const { return shells.size(); }
-    size_t nbf() const {
-      return
-      std::accumulate( shells.begin()
-                     , shells.end()
-                     , 0
-                     , [&](size_t i, const Shell &s){ return i + s.size(); }
-                     );
-    }
-  };
-
-  typedef std::vector<Basis> BasisSet;
 
   struct BasisSetParser {
     std::smatch match;
