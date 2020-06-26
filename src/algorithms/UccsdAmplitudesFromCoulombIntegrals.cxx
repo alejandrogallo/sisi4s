cc8a7bab970a7b6f685959bd10c2eba7e632942e
Alejandro Gallo <aamsgallo@gmail.com>
2020-06-26 15:10:16 +0200

    Uccsd: tidy

--- src/algorithms/UccsdAmplitudesFromCoulombIntegrals.cxx
+++ src/algorithms/UccsdAmplitudesFromCoulombIntegrals.cxx
@@ -1,8 +1,6 @@
 #include <algorithms/UccsdAmplitudesFromCoulombIntegrals.hpp>
 #include <algorithms/SimilarityTransformedHamiltonian.hpp>
 #include <unistd.h>
-#include <tcc/Tcc.hpp>
-#include <tcc/DryMachineTensor.hpp>
 #include <math/MathFunctions.hpp>
 #include <math/ComplexTensor.hpp>
 #include <math/RandomTensor.hpp>
@@ -13,7 +11,6 @@
 #include <Cc4s.hpp>
 
 using namespace cc4s;
-using namespace tcc;
 
 ALGORITHM_REGISTRAR_DEFINITION(UccsdAmplitudesFromCoulombIntegrals);
 
@@ -28,10 +25,10 @@ UccsdAmplitudesFromCoulombIntegrals::~UccsdAmplitudesFromCoulombIntegrals() {
 void UccsdAmplitudesFromCoulombIntegrals::run() {
 
   // Set this for cluster singles doubles algorithm
-  getIntegerArgument("antisymmetrize");
-  getIntegerArgument("unrestricted");
+  setIntegerArgument("antisymmetrize", 1);
+  setIntegerArgument("unrestricted", 1);
 
-  const std::vector<std::string> allArguments =
+  checkArgumentsOrDie(
     { "intermediates" // stanton intermediates
     , "antisymmetrize", "unrestricted"
     // Solver
@@ -47,13 +44,13 @@ void UccsdAmplitudesFromCoulombIntegrals::run() {
     , "HPPHCoulombIntegrals" , "PHPPCoulombIntegrals" , "HHPHCoulombIntegrals"
     , "PPPHCoulombIntegrals" , "PHPHCoulombIntegrals" , "PHHPCoulombIntegrals"
     , "UccsdDoublesAmplitudes", "UccsdSinglesAmplitudes", "UccsdEnergy"
-    };
-  checkArgumentsOrDie(allArguments);
+    }
+  );
 
   usingIntermediates = getIntegerArgument("intermediates", 1) == 1;
   if (! usingIntermediates ) {
-    LOG(0, getAbbreviation()) <<
-      "Not using Stanton et al. intermediates, the code will be much slower."
+    LOG(0, getAbbreviation())
+      << "Not using Stanton et al. intermediates, the code will be much slower."
       << std::endl;
   }
   LOG(0, getAbbreviation())
@@ -86,56 +83,58 @@ PTR(FockVector<F>) UccsdAmplitudesFromCoulombIntegrals::getResiduumTemplate(
 ) {
   CTF::Tensor<F> *Fab, *Fij, *Fia;
 
-  if (
-    isArgumentGiven("HPFockMatrix") &&
-    isArgumentGiven("HHFockMatrix") &&
-    isArgumentGiven("PPFockMatrix")
-  ) {
-    if (iterationStep == 0){
-      LOG(0, getAbbreviation()) << "Using non-canonical orbitals" << std::endl;
+  if (  isArgumentGiven("HPFockMatrix")
+     && isArgumentGiven("HHFockMatrix")
+     && isArgumentGiven("PPFockMatrix")
+     )
+  {
+    if (iterationStep == 0) {
+      LOG(0, getAbbreviation()) << "Using non-canonical orbitals "
+                                << "since you provided FockMatrices\n";
     }
     Fia = getTensorArgument<F, CTF::Tensor<F> >("HPFockMatrix");
     Fab = getTensorArgument<F, CTF::Tensor<F> >("PPFockMatrix");
     Fij = getTensorArgument<F, CTF::Tensor<F> >("HHFockMatrix");
   } else {
-    auto epsi = getTensorArgument<double>("HoleEigenEnergies");
-    auto epsa = getTensorArgument<double>("ParticleEigenEnergies");
+    auto epsi = getTensorArgument<double>("HoleEigenEnergies")
+       , epsa = getTensorArgument<double>("ParticleEigenEnergies")
+       ;
     Fia = nullptr;
-    const int Nv(epsa->lens[0]), No(epsi->lens[0]);
-    const int vv[] = {Nv, Nv};
-    const int oo[] = {No, No};
-    const int syms[] = {NS, NS};
-    // TODO: solve this memory leak
+    const int Nv(epsa->lens[0])
+            , No(epsi->lens[0])
+            , vv[] = {Nv, Nv}
+            , oo[] = {No, No}
+            , syms[] = {NS, NS}
+            ;
+    // TODO: replace with unique_ptr and give away the hamiltonian one
+    // such pointer
     Fab = new CTF::Tensor<F>(2, vv, syms, *Cc4s::world, "Fab");
     Fij = new CTF::Tensor<F>(2, oo, syms, *Cc4s::world, "Fij");
-    CTF::Transform<double, F>(
-      std::function<void(double, F &)>([](double eps, F &f) { f = eps; })
-    ) (
-      (*epsi)["i"], (*Fij)["ii"]
-    );
-    CTF::Transform<double, F>(
-      std::function<void(double, F &)>([](double eps, F &f) { f = eps; })
-    ) (
-      (*epsa)["a"], (*Fab)["aa"]
-    );
+    CTF::Transform<double, F>([](double eps, F &f) { f = eps; })( (*epsi)["i"]
+                                                                , (*Fij)["ii"]
+                                                                );
+    CTF::Transform<double, F>([](double eps, F &f) { f = eps; })( (*epsa)["a"]
+                                                                , (*Fab)["aa"]
+                                                                );
   }
 
   // Get couloumb integrals
-  auto Vijkl(getTensorArgument<F>("HHHHCoulombIntegrals"));
-  auto Vabcd(getTensorArgument<F>("PPPPCoulombIntegrals"));
-  auto Vijka(getTensorArgument<F>("HHHPCoulombIntegrals"));
-  auto Vijab(getTensorArgument<F>("HHPPCoulombIntegrals"));
-  auto Viajk(getTensorArgument<F>("HPHHCoulombIntegrals"));
-  auto Viajb(getTensorArgument<F>("HPHPCoulombIntegrals"));
-  auto Viabc(getTensorArgument<F>("HPPPCoulombIntegrals"));
-  auto Vabij(getTensorArgument<F>("PPHHCoulombIntegrals"));
-  auto Vabic(getTensorArgument<F>("PPHPCoulombIntegrals"));
-  auto Viabj(getTensorArgument<F>("HPPHCoulombIntegrals"));
-  auto Vaibc(getTensorArgument<F>("PHPPCoulombIntegrals"));
-  auto Vijak(getTensorArgument<F>("HHPHCoulombIntegrals"));
-  auto Vabci(getTensorArgument<F>("PPPHCoulombIntegrals"));
-  auto Vaibj(getTensorArgument<F>("PHPHCoulombIntegrals"));
-  auto Vaijb(getTensorArgument<F>("PHHPCoulombIntegrals"));
+  auto Vijkl(getTensorArgument<F>("HHHHCoulombIntegrals"))
+     , Vabcd(getTensorArgument<F>("PPPPCoulombIntegrals"))
+     , Vijka(getTensorArgument<F>("HHHPCoulombIntegrals"))
+     , Vijab(getTensorArgument<F>("HHPPCoulombIntegrals"))
+     , Viajk(getTensorArgument<F>("HPHHCoulombIntegrals"))
+     , Viajb(getTensorArgument<F>("HPHPCoulombIntegrals"))
+     , Viabc(getTensorArgument<F>("HPPPCoulombIntegrals"))
+     , Vabij(getTensorArgument<F>("PPHHCoulombIntegrals"))
+     , Vabic(getTensorArgument<F>("PPHPCoulombIntegrals"))
+     , Viabj(getTensorArgument<F>("HPPHCoulombIntegrals"))
+     , Vaibc(getTensorArgument<F>("PHPPCoulombIntegrals"))
+     , Vijak(getTensorArgument<F>("HHPHCoulombIntegrals"))
+     , Vabci(getTensorArgument<F>("PPPHCoulombIntegrals"))
+     , Vaibj(getTensorArgument<F>("PHPHCoulombIntegrals"))
+     , Vaijb(getTensorArgument<F>("PHHPCoulombIntegrals"))
+     ;
 
   // Read the amplitudes Tai and Tabij
   auto Tai(amplitudes->get(0))
@@ -170,28 +169,31 @@ PTR(FockVector<F>) UccsdAmplitudesFromCoulombIntegrals::getResiduumTemplate(
    .setVaijb(Vaijb).setVabci(Vabci).setVabij(Vabij)
    // set current t-amplitudes
    .setTai(Tai.get()).setTabij(Tabij.get())
-   // set a general dressing
+   // set a general dressing, since we don't want any terms to get dropped
    .setDressing(SimilarityTransformedHamiltonian<F>::Dressing::GENERAL)
    // use stanton intermediates?
    .useStantonIntermediatesUCCSD(usingIntermediates)
    ;
 
-  // T1 equations:
-  //
-  // The singles amplitude equations are simply taking the
-  // Wai part of the \bar H and setting it to zero
-  //
+  /* T1 equations:
+   * =============
+   *
+   * The singles amplitude equations are simply taking the
+   * Wai part of the \bar H and setting it to zero
+   */
   auto Wai = H.getAI();
   (*Rai)["ai"]  = (*Wai)["ai"];
   //These are the residum equations, we have to substract them from Wai
   (*Rai)["bi"] += ( - 1.0  ) * (*Fab)["bc"] * (*Tai)["ci"];
   (*Rai)["bi"] += ( + 1.0  ) * (*Fij)["ki"] * (*Tai)["bk"];
 
-  // T2 equations:
-  //
-  // The doubles amplitude equations are simply taking the
-  // Wabij part of the \bar H and setting it to zero
-  //
+  /*
+   * T2 equations:
+   * =============
+   *
+   * The doubles amplitude equations are simply taking the
+   * Wabij part of the \bar H and setting it to zero
+   */
   auto Wabij = H.getABIJ();
   (*Rabij)["abij"]  = (*Wabij)["abij"];
   //These are the residum equations, substract them from Wabij
