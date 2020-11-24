63939f27fdad8594a787b7723c8be295b59c1e4c
Alejandro Gallo <aamsgallo@gmail.com>
2020-11-24 17:43:14 +0100

    Fix include in makefile

--- etc/make/ctf.mk
+++ etc/make/ctf.mk
@@ -3,6 +3,6 @@ CTF_PATH    ?= lib/build/${CONFIG}/ctf
 CTF_LIB     ?= -L${CTF_PATH}/lib -lctf
 CTF_INCLUDE ?= -I${CTF_PATH}/include
 
-INCLUDE += ${CTF_INCLUDE}
+CC4S_INCLUDE += ${CTF_INCLUDE}
 #LIBS    += -Wl,-Bstatic ${CTF_LIB}
 LIBS_STATIC  += ${CTF_LIB}
--- etc/make/libint.mk
+++ etc/make/libint.mk
@@ -3,5 +3,5 @@ LIBINT_PATH    ?= lib/build/${CONFIG}/libint
 LIBINT_LIB     ?= -L${LIBINT_PATH}/lib -lint2
 LIBINT_INCLUDE ?= -I${LIBINT_PATH}/include/ -I${LIBINT_PATH}/include/libint2
 
-INCLUDE        += ${LIBINT_INCLUDE}
+CC4S_INCLUDE        += ${LIBINT_INCLUDE}
 LIBS_STATIC    += ${LIBINT_LIB}
