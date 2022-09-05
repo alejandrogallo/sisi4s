b88f037bee2d138a34c39d2a098e3da0774a6306
Alejandro Gallo <aamsgallo@gmail.com>
2022-09-05 12:50:02 +0200

    Remove README

--- src/algorithms/ComplexTensorReader.hpp
+++ src/algorithms/ComplexTensorReader.hpp
@@ -1,23 +1,23 @@
 /*Copyright (c) 2016, Andreas Grueneis and Felix Hummel, all rights reserved.*/
-#ifndef COMPLEX_TENSOR_READER_DEFINED 
-#define COMPLEX_TENSOR_READER_DEFINED
+#ifndef SRC_ALGORITHMS_COMPLEXTENSORREADER_HPP_
+#define SRC_ALGORITHMS_COMPLEXTENSORREADER_HPP_
+#include <vector>
 
 #include <algorithms/Algorithm.hpp>
 
 namespace cc4s {
-  class ComplexTensorReader: public Algorithm {
-  public:
-    ALGORITHM_REGISTRAR_DECLARATION(ComplexTensorReader);
-    ComplexTensorReader(
-      std::vector<Argument> const &argumentList
-    );
-    virtual ~ComplexTensorReader();
-    /**
-     * \brief Reads real tensor data into the tensor Data.
-     */
-    virtual void run();
-  };
-}
 
-#endif
+class ComplexTensorReader: public Algorithm {
+ public:
+  ALGORITHM_REGISTRAR_DECLARATION(ComplexTensorReader);
+  explicit ComplexTensorReader(std::vector<Argument> const &argumentList);
+  virtual ~ComplexTensorReader();
+  /**
+    * \brief Reads real tensor data into the tensor Data.
+    */
+  virtual void run();
+};
 
+}  // namespace cc4s
+
+#endif  //  SRC_ALGORITHMS_COMPLEXTENSORREADER_HPP_
--- src/algorithms/TensorReader.cxx
+++ src/algorithms/TensorReader.cxx
@@ -1,66 +1,69 @@
 /*Copyright (c) 2018, Andreas Grueneis and Felix Hummel, all rights reserved.*/
-#include <algorithms/TensorReader.hpp>
+#include <fstream>
+#include <vector>
+#include <string>
+
 #include <util/TensorIo.hpp>
 #include <util/Log.hpp>
 #include <Cc4s.hpp>
-#include <fstream>
 #include <util/CTF.hpp>
 #include <util/Emitter.hpp>
+#include <algorithms/TensorReader.hpp>
 
-using namespace CTF;
-using namespace cc4s;
 
-ALGORITHM_REGISTRAR_DEFINITION(TensorReader);
+template<typename F>
+using Tensor = CTF::Tensor<F>;
 
-TensorReader::TensorReader(
-  std::vector<Argument> const &argumentList
-): Algorithm(argumentList) {
-}
+namespace cc4s {
 
-TensorReader::~TensorReader() {
-}
+  ALGORITHM_REGISTRAR_DEFINITION(TensorReader);
 
-void TensorReader::run() {
-  std::string name(getArgumentData("Data")->getName());
+  TensorReader::TensorReader(std::vector<Argument> const &argumentList)
+    : Algorithm(argumentList) {}
 
-  // make sure all processes start reading the file at the same time in case
-  // it has been modified before
-  MPI_Barrier(Cc4s::world->comm);
-
-  int64_t precision(getIntegerArgument("precision", 64));
-  switch (precision) {
-  case 64:
-    allocatedTensorArgument<Float64>(
-      "Data", read<Float64>(name)
-    );
-    break;
+  TensorReader::~TensorReader() {
   }
-}
 
-template <typename F>
-Tensor<F> *TensorReader::read(const std::string &name) {
-  Tensor<F> *A;
-  std::string mode(getTextArgument("mode", "text"));
-  if (mode == "binary") {
-    std::string fileName(getTextArgument("file", name + ".bin"));
-    EMIT() << YAML::Key << "file" << YAML::Value << fileName;
-    A = TensorIo::readBinary<F>(fileName);
-  } else {
-    std::string fileName(getTextArgument("file", name + ".dat").c_str());
-    std::string delimiter(getTextArgument("delimiter", " "));
-    int64_t bufferSize(getIntegerArgument("bufferSize", 128l*1024*1024));
-    A = TensorIo::readText<F>(fileName, delimiter, bufferSize);
-    EMIT() << YAML::Key << "file" << YAML::Value << fileName;
+  void TensorReader::run() {
+    std::string name(getArgumentData("Data")->getName());
+
+    // make sure all processes start reading the file at the same time in case
+    // it has been modified before
+    MPI_Barrier(Cc4s::world->comm);
+
+    int64_t precision(getIntegerArgument("precision", 64));
+    switch (precision) {
+    case 64:
+      allocatedTensorArgument<Float64>("Data", read<Float64>(name));
+      break;
+    }
   }
-  A->set_name(name.c_str());
-  EMIT() << YAML::Key << "Data"  << YAML::Value << name;
 
-  int64_t indexCount(1);
-  for (int dim(0); dim < A->order; ++dim) {
-    indexCount *= A->lens[dim];
+  template <typename F>
+  Tensor<F> *TensorReader::read(const std::string &name) {
+    Tensor<F> *A;
+    std::string mode(getTextArgument("mode", "text"));
+    if (mode == "binary") {
+      std::string fileName(getTextArgument("file", name + ".bin"));
+      EMIT() << YAML::Key << "file" << YAML::Value << fileName;
+      A = TensorIo::readBinary<F>(fileName);
+    } else {
+      std::string fileName(getTextArgument("file", name + ".dat").c_str());
+      std::string delimiter(getTextArgument("delimiter", " "));
+      int64_t bufferSize(getIntegerArgument("bufferSize", 128l*1024*1024));
+      A = TensorIo::readText<F>(fileName, delimiter, bufferSize);
+      EMIT() << YAML::Key << "file" << YAML::Value << fileName;
+    }
+    A->set_name(name.c_str());
+    EMIT() << YAML::Key << "Data"  << YAML::Value << name;
+
+    int64_t indexCount(1);
+    for (int dim(0); dim < A->order; ++dim) {
+      indexCount *= A->lens[dim];
+    }
+    EMIT() << YAML::Key << "elements" << YAML::Value << indexCount;
+
+    return A;
   }
-  EMIT() << YAML::Key << "elements" << YAML::Value << indexCount;
 
-  return A;
-}
-
+}  // namespace cc4s
