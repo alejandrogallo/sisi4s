4a2b23cbfb07a2bc7286770e5cf04425316a8353
Alejandro Gallo <aamsgallo@gmail.com>
2022-09-15 11:07:52 +0200

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+cd `git rev-parse --show-toplevel`
+
+type -a autoreconf > /dev/null ||
+{
+        cat <<EOF && exit
+
+You don't seem to have autotools installed, please install it.
+
+  - https://www.gnu.org/software/autoconf/
+  - https://www.gnu.org/software/automake/
+
+EOF
+}
+
+
+cat <<EOF
+
+  Creating configure script
+
+EOF
+
+
+autoreconf -vif .
+test -f configure || {
+  cat <<EOF
+
+  An error happened and a configure script could not be built!
+
+EOF
+  exit 1
+}
+
+
+cat <<EOF
+
+  Now you can build by doing
+
+  mkdir build
+  cd build
+  ../configure
+  make extern
+  make all
+
+EOF
