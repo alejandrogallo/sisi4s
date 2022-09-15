e4bfecced9ae592e52842be3a39465427451b79f
Alejandro Gallo <aamsgallo@gmail.com>
2022-09-15 16:08:59 +0200

    Update the main page of doxygen

--- src/Sisi4s.hpp
+++ src/Sisi4s.hpp
@@ -1,4 +1,17 @@
-/*Copyright (c) 2015, Andreas Grueneis and Felix Hummel, all rights reserved.*/
+/* Copyright 2021 cc4s.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef SISI4S_DEFINED
 #define SISI4S_DEFINED
 
@@ -30,27 +43,18 @@ namespace sisi4s {
 
 #endif
 
-/*! \mainpage 
+/*!  * \mainpage
  * \section intro Introduction
- * 
+ *
  * Coupled Cluster For Solids (SISI4S) is is a parallel quantum
  * chemistry package built on the Cyclops Tensor Framework which
  * provides high-performance structured tensor operations. SISI4S is
  * primarily focused on iterative CC methods such as CCD, CCSD, and
  * CCSD(T) for periodic systems in a plane-wave basis. The code is
  * currently interfaced to the Vienna Ab-inition Simulation Package.
- * 
- * The software is available on
- * https://hulldamage.org/sisi4s.org/doku.php and maybe obtained
- * via the command 
  *
- * git clone git@gitlab.com:grueneis/sisi4s.git
+ * The software is available on https://github.com/alejandrogallo/sisi4s
+ * and maybe obtained via the command
  *
- * For information or access to the repository please contact <mailto:andreas.grueneis@tuwien.ac.at>
- *
- * SISI4S depends on the CTF and requires MPI to be built as the main parallel execution and communication mechanism.
- *
- * \section algorithms Algorithms
- *
- *The main functionality is based on different algorithms.
+ *   git clone git@github.com:alejandrogallo/sisi4s.git
  */
