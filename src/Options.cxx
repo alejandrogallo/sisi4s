43e0766eb2e2cfbaf4456e125fde06b1bc950f29
Alejandro Gallo <aamsgallo@gmail.com>
2022-09-18 18:43:20 +0200

    Improve error checking in Options

--- src/Options.cxx
+++ src/Options.cxx
@@ -1,5 +1,4 @@
 /*Copyright (c) 2015, Andreas Grueneis and Felix Hummel, all rights reserved.*/
-
 #include <Options.hpp>
 #include <string>
 #include <sstream>
@@ -9,7 +8,7 @@ using namespace sisi4s;
 Options::Options(int argc_, char **argv_)
   : app{"SiSi4S: Coupled Cluster For Solids"}
   , logLevel(Options::DEFAULT_LOG_LEVEL)
-  , inFile("sisi4s.in")
+  , inFile("")
   , logFile("sisi4s.log")
   , yamlOutFile("sisi4s.out.yaml")
   , argc(argc_)
@@ -19,24 +18,35 @@ Options::Options(int argc_, char **argv_)
   , dryRun(false)
   , eclFile("")
   {
+
     app.add_option("-i,--in", inFile, "Input file path")
-      ->default_val(inFile);
+      ->check(CLI::ExistingFile)
+      ->required(true);
+
     app.add_option("-o,--out", yamlOutFile, "Output yaml file")
       ->default_val(yamlOutFile);
+
     app.add_option("-l,--log", logFile, "Output log file")
       ->default_val(logFile);
-    app.add_option("--dry", dryRun, "Do a dry run pass")
+
+    app.add_flag("--dry", dryRun, "Do a dry run pass")
       ->default_val(dryRun);
+
     app.add_option("--log-level", logLevel, "Log level")
       ->default_val(logLevel);
+
     app.add_flag("--cc4s", cc4s,
                  "Interpret the input file in the old cc4s DSL")
       ->default_val(cc4s);
+
     app.add_flag("--list-algorithms,--list", listAlgorithms,
                  "List registered algorithms")
       ->default_val(listAlgorithms);
+
     app.add_option("--load", eclFile, "ECL script to load")
+      ->check(CLI::ExistingFile)
       ->default_val(eclFile);
+
 }
 
 int Options::parse() {
@@ -44,6 +54,7 @@ int Options::parse() {
     (app).parse((argc), (argv));
   } catch(const CLI::ParseError &e) {
     (app).exit(e);
+    std::exit(1);
   }
   if (app.get_help_ptr()->count()) std::exit(0);
   return 0;
