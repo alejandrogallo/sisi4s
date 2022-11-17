e45d12b425e74be7ea0f8a488ab282f76dadbbbe
Alejandro Gallo <aamsgallo@gmail.com>
2022-11-17 13:03:33 +0100

    Add Let algorithm

---
+++ src/algorithms/Let.cxx
@@ -0,0 +1,5 @@
+#include<algorithms/Let.hpp>
+
+using namespace sisi4s;
+IMPLEMENT_ALGORITHM(Let) {
+}
---
+++ src/algorithms/Let.hpp
@@ -0,0 +1,12 @@
+#ifndef LET_HPP_
+#define LET_HPP_
+
+#include <algorithms/Algorithm.hpp>
+
+namespace sisi4s {
+
+  DEFINE_ALGORITHM_HEADER(Let,);
+
+}
+
+#endif // EXIT_ALGORITHM_DEFINED_HERE
